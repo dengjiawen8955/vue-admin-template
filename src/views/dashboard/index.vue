5fadbf18e17bdcbfbcc6ec44f1d3143d252f6a2d
dengjiawen <dengjiawen8955@gmail.com>
2023-01-31 20:12:50 +0800

    feat: 新增 clieint

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,6 +2,10 @@
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
     <div class="dashboard-text">Hello</div>
+    <!-- 选择图片按钮 -->
+    <el-button type="primary" @click="chooseImage">选择图片</el-button>
+    <!-- 上传图片按钮 -->
+    <el-button type="primary" @click="uploadImage">提交图片</el-button>
   </div>
 </template>
 
@@ -14,6 +18,29 @@ export default {
     ...mapGetters([
       'name'
     ])
+  },
+  data() {
+    return {
+      file: null,
+    }
+  },
+  methods: {
+    // 选择图片
+    chooseImage() {
+      this.$refs.file.click()
+    },
+    // 上传图片
+    uploadImage() {
+      const formData = new FormData()
+      formData.append('file', this.file)
+      this.$axios.post('/upload', formData).then(res => {
+        console.log(res)
+      })
+    },
+    // 选择图片后的回调
+    handleFileChange(e) {
+      this.file = e.target.files[0]
+    }
   }
 }
 </script>
